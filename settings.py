217067d42519f8ba7b6cd7751c1c227d37ff5add
mephistiks <44960783+mephistiks@users.noreply.github.com>
2022-12-18 23:43:04 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 DB_LINK="mongodb://mongo:mongo@localhost:27020/?authMechanism=DEFAULT"
 
-API_KEY="5269257453:AAFdcipJQegLvMRYIMayS9OHd909fh0Fk44"
+API_KEY="api"
 
 redis_host = "127.0.0.1"
 redis_port = 6401
